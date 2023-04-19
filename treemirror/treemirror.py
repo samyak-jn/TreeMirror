c295742c737f41cc29cf88099b40f8391c8432c3
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-04-19 04:58:34 +0300

    feat: pre-commit-config yaml file added feat: formatter configs are added into pyproject toml file chore: code-refomatting complete docs: code docs lines fixed for pre-commit chore: lock file updated
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- treemirror/treemirror.py
+++ treemirror/treemirror.py
@@ -1,34 +1,36 @@
 import os
 import shutil
+
 from tqdm import tqdm
 
 
-def process_directory_structure(path):
+def process_dir_struct(path):
     print(f"Directory structure of {path}:")
-    print_directory_structure(path, 0)
+    print_dir_struct(path, 0)
 
-def print_directory_structure(path, indent=0):
+
+def print_dir_struct(path, indent=0):
     for file in os.listdir(path):
         print(f"{' ' * indent}- {file}")
         full_path = os.path.join(path, file)
         if os.path.isdir(full_path):
-            print_directory_structure(full_path, indent + 2)
+            print_dir_struct(full_path, indent + 2)
 
-def copy_dir_structure(src_dir, dst_dir, prompt_overwrite=True):
+
+def copy_dir_struct(src_dir, dst_dir, prompt_overwrite=True):
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
 
     files = os.listdir(src_dir)
-    for item in tqdm(files, desc='Copying directory structure', unit='file'):
+    for item in tqdm(files, desc="Copying directory structure", unit="file"):
         src_path = os.path.join(src_dir, item)
         dst_path = os.path.join(dst_dir, item)
 
         if os.path.isdir(src_path):
-            copy_dir_structure(src_path, dst_path, prompt_overwrite)
+            copy_dir_struct(src_path, dst_path, prompt_overwrite)
         else:
             if os.path.exists(dst_path) and prompt_overwrite:
                 overwrite = input(f"Do you want to overwrite {dst_path}? (y/n): ")
                 if overwrite.lower() == "n":
                     continue
             shutil.copy2(src_path, dst_path)
-
