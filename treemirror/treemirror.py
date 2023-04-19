70300facdb448cc8a66022dda654638d384ef1a4
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-04-19 04:30:45 +0300

    feat: cli added fix: argparse removed fix: lock file created
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- treemirror/treemirror.py
+++ treemirror/treemirror.py
@@ -1,36 +1,7 @@
 import os
-import argparse
 import shutil
 from tqdm import tqdm
 
-def main():
-    parser = argparse.ArgumentParser(description="Mirror the directory structure of a source directory to a destination directory.")
-    parser.add_argument('path', type=str, help='path to project directory')
-    parser.add_argument('output_path', type=str, help='path to output directory')
-    args = parser.parse_args()
-
-    # Check if input directory exists
-    if not os.path.isdir(args.path):
-        print(f"Error: {args.path} is not a directory.")
-        return
-
-    # Check if output directory exists; create it if it doesn't
-    if not os.path.isdir(args.output_path):
-        os.makedirs(args.output_path)
-    else:
-        # Prompt user before overwriting files
-        overwrite = input(f"{args.output_path} already exists. Overwrite files? (y/n): ")
-        if overwrite.lower() != 'y':
-            return
-
-    # Process directory structure
-    process_directory_structure(args.path)
-
-    # Ask user if they want to copy the directory structure to the specified output directory
-    copy_structure = input("Do you want to copy this structure to the specified output directory? (y/n): ")
-    if copy_structure.lower() == 'y':
-        copy_dir_structure(args.path, args.output_path)
-        print(f"Directory structure copied to {args.output_path}.")
 
 def process_directory_structure(path):
     print(f"Directory structure of {path}:")
@@ -61,5 +32,3 @@ def copy_dir_structure(src_dir, dst_dir, prompt_overwrite=True):
                     continue
             shutil.copy2(src_path, dst_path)
 
-if __name__ == '__main__':
-    main()
